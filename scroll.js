3ff992d9742885caf64276d07212de925830829a
stsanchez22 <stsanchez@gmail.com>
2015-02-01 09:20:35 -0800

    HTML Sync

--- scroll.js
+++ scroll.js
@@ -6,7 +6,7 @@ $(function() {
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 1250);
         return false;
       }
     }
